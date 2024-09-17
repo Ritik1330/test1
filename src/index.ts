3071889075ff355e3b7526a74d7a99fc5e8872d6
Ritik Kumar Sharma <ritikcollege@gmail.com>
2024-09-17 14:08:30 +0530

    json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
 
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
-})
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Express Typescript on Vercel");
+});
 
-app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓')
-})
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
+});
+
+app.get("/json", (_req: Request, res: Response) => {
+  return res.status(200).json({
+    success: true,
+    message: "Category Deleted Succesfully",
+  });
+});
 
 app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`)
-})
+  return console.log(`Server is listening on ${port}`);
+});
