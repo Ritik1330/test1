e966ba05974f1dc05a970ff840d5dc71ad3876ff
Ritik Kumar Sharma <ritikcollege@gmail.com>
2024-09-17 23:52:19 +0530

    json1

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
@@ -18,6 +19,47 @@ app.get("/json", (_req, res) => {
         message: "Category Deleted Succesfully",
     });
 });
+const multer_1 = __importDefault(require("multer"));
+const cloudinary_1 = require("cloudinary");
+const dotenv_1 = __importDefault(require("dotenv"));
+const streamifier_1 = __importDefault(require("streamifier"));
+dotenv_1.default.config();
+const storage = multer_1.default.memoryStorage();
+const upload = (0, multer_1.default)({ storage });
+const uploadMiddleware = upload.single("file");
+cloudinary_1.v2.config({
+    cloud_name: "dbacwthnv",
+    api_key: "511772263679235",
+    api_secret: "0opOgfJbWCdJmdzQHjka-eMjVXc",
+    secure: true,
+});
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+            return resolve(result);
+        });
+    });
+}
+app.get("/img", async (req, res) => {
+    await runMiddleware(req, res, uploadMiddleware);
+    console.log(req.file.buffer);
+    const stream = await cloudinary_1.v2.uploader.upload_stream({
+        folder: "demo",
+    }, (error, result) => {
+        if (error)
+            return console.error(error);
+        res.status(200).json(result);
+    });
+    streamifier_1.default.createReadStream(req.file.buffer).pipe(stream);
+});
+exports.config = {
+    api: {
+        bodyParser: false,
+    },
+};
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,51 @@ app.get("/json", (_req: Request, res: Response) => {
   });
 });
 
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+import streamifier from "streamifier";
+
+dotenv.config();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const uploadMiddleware = upload.single("file");
+cloudinary.config({
+  cloud_name: "dbacwthnv",
+  api_key: "511772263679235",
+  api_secret: "0opOgfJbWCdJmdzQHjka-eMjVXc",
+  secure: true,
+});
+function runMiddleware(req: any, res: any, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+app.get("/img", async (req: any, res: Response) => {
+  await runMiddleware(req, res, uploadMiddleware);
+  console.log(req.file.buffer);
+  const stream = await cloudinary.uploader.upload_stream(
+    {
+      folder: "demo",
+    },
+    (error, result) => {
+      if (error) return console.error(error);
+      res.status(200).json(result);
+    }
+  );
+  streamifier.createReadStream(req.file.buffer).pipe(stream);
+});
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);
 });
